f1fc73fb87e0e6b69ae3fdc5cce5c0480a808ea8
Anb <quizbharatofficial@gmail.com>
2026-08-03 18:53:21 +0000

    Fix Android Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-android {
-    namespace = "com.selfcleaner.app"
-    compileSdk = 35
-    defaultConfig {
-        applicationId = "com.selfcleaner.app"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
